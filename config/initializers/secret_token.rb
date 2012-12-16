15d9d167b3d0d9233a26d91adc6b31fdb5807952
cabunney <cabunney@gmail.com>
2012-12-16 02:58:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoultonbunneyCom::Application.config.secret_token = '9265d3acbcdd6a9ab909a92fd7c1694f68bb5226abaf86b6f18ac3d5c09fd9fb74439e9ec2c34b3cbeef1bdd6d399052ffedcfbb8a053323d542f7a087e8f9a0'
