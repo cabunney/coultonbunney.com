4eb66856519b4eaa662f3e454089667002da2a35
Coulton Bunney <cabunney@gmail.com>
2023-03-15 23:03:53 -0400

    updated

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/all .css
+//= link_directory ../stylesheets/casestudy .css
 //= link_tree ../videos
